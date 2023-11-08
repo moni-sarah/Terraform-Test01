81996ab8e54cee2905d511c9a3b735c5bd855d04
Sarah Monica <108329964+moni-sarah@users.noreply.github.com>
2023-11-08 09:43:54 +0000

    Add files

---
+++ backend/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.79.0"
+    }
+  }
+}
+
+provider "azurerm" {
+features {
+
+}
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
